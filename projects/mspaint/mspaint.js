da8d1ff0ae3f9ec5ca24c68059632ac99f26facf
lindseysamp <lindseysamp77@gmail.com>
2025-03-16 23:44:32 -0500

    Update mspaint.js

--- projects/mspaint/mspaint.js
+++ projects/mspaint/mspaint.js
@@ -1,1 +1,13 @@
+const select = (query, parent = document) => [...parent.querySelectorAll(query)]
+      const selectOne = (query, parent = document) => parent.querySelector(query)
 
+      select('.palette__color').forEach(
+        element => element.style.backgroundColor = element.dataset.color
+      )
+
+      selectOne('.tools__box').addEventListener('click', ({ target }) => {
+        if (target.nodeName === 'BUTTON') {
+          target.classList.remove('win-border')
+          target.classList.add('win-border-inset')
+        }
+      })
